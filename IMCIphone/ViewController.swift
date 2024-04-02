a450f448c1ed6bfda9b2dbaf9a00183acb3c792f
mananas <jesus.esteban@outlook.com>
2024-04-02 12:45:46 +0200

    Initial Commit

---
+++ IMCIphone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMCIphone
+//
+//  Created by Mañanas on 2/4/24.
+//
+
+import UIKit
+// Prueba para comit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
