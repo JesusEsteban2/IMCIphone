0f206e8b0d3d8ae8c0cebe5aa7394c04e610b016
jesusesteban2 <jesus.esteban@outlook.com>
2024-04-02 21:22:05 +0200

    2 commit

--- IMCIphone/ViewController.swift
+++ IMCIphone/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 // Prueba para comit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelAltura: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonHelloWorld(_ sender: Any) {
+        labelAltura.text="Hola Mundo"
+    }
+    
+    @IBAction func buttonBack(_ sender: Any) {
+        labelAltura.text="Altura"
+    }
+    
+    @IBAction func buttonTint(_ sender: Any) {
+        labelAltura.text="gordura"
+        
+    }
+    
+    
+    
 }
+
+    
+
 
