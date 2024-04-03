0587009037f5d17cc52b36a9fc64b6793e71b771
jesusesteban2 <jesus.esteban@outlook.com>
2024-04-03 21:59:15 +0200

    3

--- IMCIphone/ViewController.swift
+++ IMCIphone/ViewController.swift
@@ -10,18 +10,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelAltura: UILabel!
-
     @IBOutlet weak var resultIMC: UILabel!
-
-    @IBOutlet weak var editAltura: UITextField!;
-
+    @IBOutlet weak var editAltura: UITextField!
     @IBOutlet weak var editPeso: UITextField!
-    
     @IBOutlet weak var stepperAltura: UIStepper!
-    
     @IBOutlet weak var stepperPeso: UIStepper!
-    
     @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var buttonMostrar: UIButton!
+    @IBOutlet weak var buttonBack: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,19 +26,23 @@ class ViewController: UIViewController {
         stepperPeso.value=70
     }
     
-    @IBAction func buttonHelloWorld(_ sender: Any) {
+    @IBAction func buttonMostrar(_ sender: Any) {
         imagen.isHidden=false
+        buttonMostrar.isHidden=true
+        buttonBack.isHidden=false
     }
+  
     
     @IBAction func buttonBack(_ sender: Any) {
         imagen.isHidden=true
-        
+        buttonMostrar.isHidden=false
+        buttonBack.isHidden=true
     }
     
     @IBAction func buttonTint(_ sender: Any) {
         
         var altura: Double? = Double(editAltura.text!)
-        var peso:Double? = Double (editPeso.text!)
+        let peso:Double? = Double (editPeso.text!)
         var imc:Double=0
         if altura != nil {
             altura!=altura!/100
@@ -56,16 +56,22 @@ class ViewController: UIViewController {
         switch imc {
         case 0...18.5:
             resultIMC.backgroundColor=UIColor.blue
+            resultIMC.textColor=UIColor.white
         case 18.5...24.9:
-            resultIMC.backgroundColor=UIColor.blue
+            resultIMC.backgroundColor=UIColor(resource:ColorResource.bluePer)
+            resultIMC.textColor=UIColor.black
         case 25.0...29.9:
             resultIMC.backgroundColor=UIColor.green
+            resultIMC.textColor=UIColor.black
         case 30.0...34.9:
-            resultIMC.backgroundColor=UIColor.green
+            resultIMC.backgroundColor=UIColor(resource:ColorResource.greenYel)
+            resultIMC.textColor=UIColor.black
         case 35.0...39.9:
             resultIMC.backgroundColor=UIColor.orange
+            resultIMC.textColor=UIColor.black
         default:
             resultIMC.backgroundColor=UIColor.red
+            resultIMC.textColor=UIColor.white
         }
         
         
